f26c810f0ecbf1da06b9fe56ca38265fa5642a56
Tim P <tim.priestley.2020@gmail.com>
2026-03-18 15:30:23 +0800

    updated app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import Calculator from './components/Calculator';
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-900 to-black flex flex-col items-center justify-center p-4">
-      <div className="text-center">
+      <div className="flex flex-col items-center">
         <h1 className="text-5xl font-bold text-white mb-2">Addition.site</h1>
         <p className="text-gray-400 mb-8">Your Modern Calculator</p>
         <Calculator />
 
-        {/* Ad spacer + Adsterra banner */}
-        <div className="mt-8">
+        {/* Adsterra banner - locked to calculator width */}
+        <div className="mt-12" style={{ width: '384px' }}>
           <div id="container-38d4986bf6dea79bb7233722f8c2b358" />
         </div>
       </div>
